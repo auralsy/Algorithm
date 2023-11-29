25f26010123762c1b9a5543e9634ea4832c45df6
auralsy <147060267+auralsy@users.noreply.github.com>
2023-11-30 08:56:56 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	if ((n % 4 == 0) && (n % 100 != 0))
+		printf("%d", 1);
+	else if ((n % 400) == 0)
+		printf("%d", 1);
+	else
+		printf("%d", 0);
+}
