6ea06a09c543fa33e24c8ea56d533ee02ee2dd9a
auralsy <147060267+auralsy@users.noreply.github.com>
2024-05-20 18:51:14 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string f = ":fan:";
+
+	string h;
+	cin >> h;
+
+	cout << f << f << f<<'\n';
+	cout << f << ":" << h << ":" << f<<'\n';
+	cout << f << f << f;
+}
