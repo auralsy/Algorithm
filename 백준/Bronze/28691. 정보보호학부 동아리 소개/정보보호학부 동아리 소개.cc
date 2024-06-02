e1964217b82844a75a16159bcbf30c30082af74b
auralsy <147060267+auralsy@users.noreply.github.com>
2024-06-03 00:32:39 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char s;
+
+	cin >> s;
+
+	if (s == 'M')
+	{
+		cout << "MatKor";
+	}
+	else if (s == 'W')
+	{
+		cout << "WiCys";
+	}
+	else if (s == 'C')
+	{
+		cout << "CyKor";
+	}
+	else if (s == 'A')
+	{
+		cout << "AlKor";
+	}
+	else
+		cout << "$clear";
+}
