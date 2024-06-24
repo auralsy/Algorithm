1f1d1c4337514289df2f4124a48f85f341b71513
auralsy <147060267+auralsy@users.noreply.github.com>
2024-06-24 23:34:54 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -1,23 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < n; i++) //줄반복
-	{
-		for (int j = 0; j < i; j++) //공백 반복
-		{
-			cout << " ";
-		}
-		
-		for (int t = n - i; t > 0; t--) //별 반복(n-i개)
-		{
-			cout << "*";
+	for (int i = n; i > 0; i--) {
+
+		for (int j = 0; j < n-i; j++) {
+			cout << ' ';
 		}
 
-		cout << '\n';
+		for (int j = 0; j < i; j++) {
+			cout << '*';
+		}
+		cout << endl;
 	}
+
+	return 0;
 }
