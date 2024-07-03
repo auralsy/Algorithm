1afc1f01ee12d2310a1e5242be9580009d7a91e9
auralsy <147060267+auralsy@users.noreply.github.com>
2024-07-03 20:53:52 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char s;
+
+	cin >> s;
+
+	if (s == 'N')
+		cout << "Naver D2";
+	else if (s == 'n')
+		cout << "Naver D2";
+	else
+		cout << "Naver Whale";
+}
