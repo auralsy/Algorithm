cc8fe1dedeebb9fba55b76e17d5fa5cfa61d9b89
auralsy <147060267+auralsy@users.noreply.github.com>
2024-10-04 21:08:03 +0900

    [Bronze V] Title: SciComLove, Time: 164 ms, Memory: 17652 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException
+	{
+		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 int n = sc.nextInt();
+		 
+		 String str = "SciComLove";
+		 
+		 if(n == 1)
+		 {
+			 System.out.println(str);
+		 }
+		 else
+		 {
+			 System.out.println(str);
+			 System.out.println(str);
+		 }
+	         
+	}
+}
