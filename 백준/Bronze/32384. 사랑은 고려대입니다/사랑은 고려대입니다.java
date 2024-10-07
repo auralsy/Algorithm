cbe8b406349f3d301108e1f6205bc1efdb2bd1c7
auralsy <147060267+auralsy@users.noreply.github.com>
2024-10-07 11:51:38 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException
+	{
+		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 int n = sc.nextInt();
+		 
+		 String str = "LoveisKoreaUniversity";
+		 
+		 for(int i = 0; i < n; i++)
+		 {
+			 System.out.print(str + " ");
+		 }
+	}
+}
