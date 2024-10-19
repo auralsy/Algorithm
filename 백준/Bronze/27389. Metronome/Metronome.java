9492a6d715a63673dcd5fe2cd64060ef9a97da87
auralsy <147060267+auralsy@users.noreply.github.com>
2024-10-19 15:23:53 +0900

    [Bronze V] Title: Metronome, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		double r = (n / 4.0);
+		
+		System.out.printf("%.2f", r);
+	}
+}
