7253f8f4acc535c26fabdd6ca5b3b7e8a3e4544b
aura_230 <147060267+auralsy@users.noreply.github.com>
2024-11-04 22:09:00 +0900

    [Bronze V] Title: Multiply, Time: 592 ms, Memory: 56312 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -6,14 +6,15 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException
 	{
-		Scanner sc = new Scanner(System.in);
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //숫자랑 연산 커지니까 BufferedReader로
+		String hoo_ha = bf.readLine(); //첫 줄은 그냥 문자열 자체로 입력 받아
+		//BufferedReader로 입력받으면 문자열로 취급되는데 new BigInteger(String) 생성자가 입력받은 문자열을 BigInteger로 변환
+		BigInteger a = new BigInteger(bf.readLine());
+		BigInteger b = new BigInteger(bf.readLine());
 		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		long a = sc.nextInt();
-		long b = sc.nextInt();
-		
-		System.out.println(a * b);
+		System.out.println(a.multiply(b)); //곱셈 수행
+		//multiply 메서드
+		// -> BigInteger의 메서드
+		// -> BigInteger 객체를 곱하여 새로운 BigInteger 객체를 반환
 	}
 }
