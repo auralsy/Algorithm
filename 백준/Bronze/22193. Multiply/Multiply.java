3d86b1b7e75781a533efd379cd82e17a1d5f7c77
aura_230 <147060267+auralsy@users.noreply.github.com>
2024-11-04 21:48:22 +0900

    [Bronze V] Title: Multiply, Time: 180 ms, Memory: 17632 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		
+		System.out.println(a * b);
+	}
+}
