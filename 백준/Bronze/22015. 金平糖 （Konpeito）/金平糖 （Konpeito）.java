316d511edb1bad3b4a970012f2f972bedb3b537c
aura_230 <147060267+auralsy@users.noreply.github.com>
2024-12-26 18:38:44 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 176 ms, Memory: 17672 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -11,13 +11,33 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         
-        if(a == b && b == c)
+        if(a == b && b == c) //다 같을 때
         {
         	System.out.println(0);
         }
-        else if(c >= b && c > a)
+        else if(c >= b && c > a) // c가 가장 클 때
         {
         	System.out.println(2*c - b - a);
+        }
+        else if(b >= c && b > a) // b가 가장 클 때
+        {
+        	System.out.println(2*b - c - a);
+        }
+        else if(a >= c && a > b) // a가 가장 클 때
+        {
+        	System.out.println(2*a - c - b);
+        }
+        else if(c > b && c >= a) // c가 가장 클 때
+        {
+        	System.out.println(2*c - b - a);
+        }
+        else if(b > c && b >= a) // b가 가장 클 때
+        {
+        	System.out.println(2*b - c - a);
+        }
+        else if(a > c && a >= b) // a가 가장 클 때
+        {
+        	System.out.println(2*a - c - b);
         }
     }
 }
