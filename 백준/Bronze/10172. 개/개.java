f956723d5efff09b2bd42951483f62e641f42a85
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-03 21:41:28 +0900

    [Bronze V] Title: 개, Time: 92 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|");
+        bw.newLine();
+        bw.write("|q p|   /}");
+        bw.newLine();
+        bw.write("( 0 )\"\"\"\\");
+        bw.newLine();
+        bw.write("|\"^\"`    |");
+        bw.newLine();
+        bw.write("||_/=\\\\__|");
+        bw.flush();
+    }
+}
