63392449a6376f6c43354f98c532d44895560c04
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-03 19:28:24 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String Line = br.readLine();
+        StringTokenizer st = new StringTokenizer(Line);
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(((a+b)%c) + "");
+        bw.newLine();
+        bw.write(((a%c) + (b%c))%c + "");
+        bw.newLine();
+        bw.write((a*b)%c + "");
+        bw.newLine();
+        bw.write(((a%c)*(b%c))%c + "");
+
+        bw.flush();
+    }
+}
