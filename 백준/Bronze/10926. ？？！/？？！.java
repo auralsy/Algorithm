1081cf5e18e26b89358bb98a249afab14c562775
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-03 19:17:21 +0900

    [Bronze V] Title: ??!, Time: 116 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String Line = br.readLine();
+
+        bw.write(Line + "??!");
+
+        bw.flush();
+    }
+}
