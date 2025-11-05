a6947105c502889eaeb72377b20098d1327e96da
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-05 21:05:58 +0900

    [Bronze II] Title: 소수 찾기, Time: 176 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,51 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main 
-{
+public class Main {
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		int[] arr = new int[n];
-		
-		for(int i = 0; i < n; i++)
-		{
-			arr[i] = sc.nextInt();
-		}
-		
-		int count = 0;
-		
-		for(int i = 0; i < n; i++)
-		{
-			if(isprime(arr[i]) == true)
-				count++;
-		}
-		
-		System.out.println(count);
-		
-	}
-	
-	public static boolean isprime(int n)
-	{
-		//0, 1은 소수 아님
-		if(n <= 1)
-			return false;
-		//2는 소수 고정
-		if(n == 2)
-			return true;
-		//짝수는 소수 아님
-		if(n % 2 == 0)
-			return false;
-		
-		for(int i = 3; i <= Math.sqrt(n); i+=2)
-		{
-			if(n % i == 0)
-				return false;
-		}
-		return true;
-	}
+    public static boolean checkPrime(int n)
+    {
+        if(n < 2)
+            return false;
 
+        for(int i = 2; i*i <= n; i++)
+        {
+            if(n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int result = 0;
+
+        for(int i = 0; i < t; i++)
+        {
+            int n = sc.nextInt();
+            
+            if(checkPrime(n))
+                result += 1;
+        }
+        System.out.println(result);
+    }
 }
