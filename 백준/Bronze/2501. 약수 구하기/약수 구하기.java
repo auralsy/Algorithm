41c51976e885ad40e405f29c22834cfafe887279
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-05 20:40:40 +0900

    [Bronze III] Title: 약수 구하기, Time: 168 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -6,19 +6,19 @@ public class Main
     public static void main(String args[]) throws IOException
     {
         Scanner sc = new Scanner(System.in);
-
+        
         int n = sc.nextInt();
         int k = sc.nextInt();
-
-        int count = 0;
+        
+        int count = 0; // k번째 찾기 위한 변수
         
         for(int i = 1; i <= n; i++)
         {
             if(n%i == 0)
             {
-                count++;
+                count += 1;  // 약수 찾으면
                 
-                if(count == k)
+                if(count == k)  // k번째 약수 찾으면
                 {
                     System.out.println(i);
                     return;
@@ -26,6 +26,6 @@ public class Main
             }
         }
         
-        System.out.println(0);
-    }
+        System.out.println(0);  // 'k번쨰 약수'를 못찾았을 때
+}
 }
