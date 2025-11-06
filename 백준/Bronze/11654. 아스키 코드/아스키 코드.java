6b1afa587a80c8882291d0e2bff20e509e4bac2f
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-06 14:17:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char c = str.charAt(0);
+        int a = (int)c;
+
+        System.out.println(a);
+
+    }
+}
