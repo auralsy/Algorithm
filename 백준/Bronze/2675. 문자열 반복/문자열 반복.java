871e980442f8548a67cbfa43f013b6b1eff63c6d
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-07 17:30:45 +0900

    [Bronze II] Title: 문자열 반복, Time: 188 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++)
-		{
-			int t = sc.nextInt();
-			String s = sc.next();
-			
-			for(int j = 0; j < s.length(); j++)
-			{
-				for(int k = 0; k < t; k++) //
-				{
-					System.out.print(s.charAt(j));
-				}
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++)
+        {
+            int n = sc.nextInt();
+            String str = sc.next();
+
+            for(int j = 0; j < str.length(); j++)
+            {
+                for(int k = 0; k < n; k++)
+                {
+                        System.out.print(str.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
 }
