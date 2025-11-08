8f5bb5d6363dc57a9fb14acfa26724cdc5996513
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-08 13:14:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,34 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int[] arr = new int[26]; //알파벳 갯수만큼의 길이 가진 배열
-		
-		//배열을 일단 -1로 초기화
-		for(int i = 0; i < arr.length; i++)
-		{
-			arr[i] = -1;
-		}
-		
-		String s = sc.nextLine(); //문자열 입력 받기
-		
-		for(int i = 0; i < s.length(); i++)
-		{
-			char ch = s.charAt(i);
-			
-			if(arr[ch - 'a'] == -1) //등장했는지 안했는지 여부
-			{
-				arr[ch - 'a'] = i; //조건 맞으면 해당 인덱스로 수정
-			}
-		}
-		
-		for(int i = 0; i < 26; i++)
-		{
-			System.out.print(arr[i] + " ");
-		}
-	}
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        int[] arr = new int[26];
+        for(int i = 0; i < 26; i++)
+        {
+            arr[i] = -1;
+        }
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            char c = str.charAt(i);
+
+            // a의 아스키코드 값 뺀 결과가 알파벳 별 위치
+            // 알파벳이 '처음' 나온 위치 기준이므로 -1일 때만 초기화
+            if(arr[c - 'a'] == -1)
+                arr[c - 'a'] = i;
+        }
+
+        for(int n : arr)
+        {
+            System.out.print(n + " ");
+        }
+    }
 }
