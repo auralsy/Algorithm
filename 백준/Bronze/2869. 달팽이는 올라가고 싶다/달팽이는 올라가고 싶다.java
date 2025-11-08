3e738dfb655b858b985f69cdb06873305dd6e097
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-08 14:39:38 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int v = sc.nextInt();
-		
-		int s = (v - b) / (a - b) + ((v-b)%(a-b) == 0? 0 : 1);
-		
-		System.out.println(s);
-	}
+        int du = sc.nextInt();
+        int nd = sc.nextInt();
+        int aim = sc.nextInt();
 
+        int day = (aim-nd)/(du-nd) + ((aim-nd)%(du-nd) == 0? 0:1);
+
+        System.out.println(day);
+    }
 }
