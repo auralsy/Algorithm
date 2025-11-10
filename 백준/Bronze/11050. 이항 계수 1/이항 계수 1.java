f4cf07df94fcd2b0554c29b8854ad4f5db035c30
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-10 12:23:22 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,29 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		
-		int u = 1, d = 1;
-		int f;
-		
-		if(k == 0)
-			f = 1;
-		else
-		{
-			for(int i = 0; i < k; i++)
-			{
-				u = u*(n-i);
-				d = d*(k-i);
-			}
-			f = u / d;
-		}
-		System.out.println(f);
-	}
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            a *= i;
+        }
+        
+        for(int i = 1; i <= k; i++)
+        {
+            b *= i;
+        }
+        
+        for(int i = 1; i <= (n-k); i++)
+        {
+            c *= i;
+        }
 
+        System.out.println(a/(b*c));
+    }
 }
