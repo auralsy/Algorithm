d25d494c92f47691b80cbbee2cd36502dc19abd7
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-10 12:15:47 +0900

    [Bronze III] Title: 팩토리얼, Time: 184 ms, Memory: 17492 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,20 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main 
-{
-	public static void main(String[] args) 
-	{
-		Scanner scanner = new Scanner(System.in);
-		
-		int k = scanner.nextInt();
-		
-		int f = 1;
-		
-		for(int i = 1; i <= k; i++)
-		{
-			f = f * i;
-		}
-		
-		System.out.println(f);
-	}
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int fr = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            fr *= i;
+        }
+
+        System.out.println(fr);
+
+    }
 }
