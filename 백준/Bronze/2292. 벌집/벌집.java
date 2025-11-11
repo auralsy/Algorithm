d77afc87f187a974f853fc7c5ce93fe8bb0a36a7
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-11 09:57:03 +0900

    [Bronze II] Title: 벌집, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,31 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main 
-{
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		int k = 1;
-		
-		for(int i = 1; i < 20000000; i++)
-		{
-			if(n == 1)
-			{
-				System.out.println(1);
-				break;
-			}
-			else if(k < n && k + 6*i >= n)
-			{
-				System.out.println(i+1);
-				break;
-			}
-			else
-				k = k + 6*i;
-		}
-	}
-	
+        int n = sc.nextInt();
+
+        int count = 1;
+
+        for(int i = 1; i < 20000000; i++)
+        {
+            if(n == 1)
+            {
+                System.out.println(1);
+                break;
+            }
+            else if(count < n && count + 6*i >= n)
+            {
+                System.out.println(i+1);
+                break;
+            }
+            else
+                count = count + 6*i;
+        }
+    }
 }
