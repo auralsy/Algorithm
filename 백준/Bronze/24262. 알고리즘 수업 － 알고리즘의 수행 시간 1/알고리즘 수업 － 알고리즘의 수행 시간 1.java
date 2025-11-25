c4d14474ea4323b1d19ca11256d06cfae3f63a95
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-25 21:00:30 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
