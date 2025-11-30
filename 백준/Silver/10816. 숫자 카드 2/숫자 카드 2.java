c9b12f0b2c92c064b89bbb5557540592981a9fe5
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-11-30 20:30:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1060 ms, Memory: 172720 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,63 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String args[]) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException
-	{
-		
-		 Scanner sc = new Scanner(System.in);
-	        StringBuilder sb = new StringBuilder(); // 문자열 조작 가능하게 하는 class
+        int n = Integer.parseInt(br.readLine());
 
-	        int N = sc.nextInt();
-	        int[] A = new int[N];
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-	        for (int i = 0; i < A.length; i++) 
-	        {
-	            A[i] = sc.nextInt();
-	        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+        {
+            int key = Integer.parseInt(st.nextToken());
 
-	        int M = sc.nextInt();
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
 
-	        Arrays.sort(A); //정렬 for 이진탐색
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++)
+        {
+            int target = Integer.parseInt(st.nextToken());
 
-	        for (int i = 0; i < M; i++) 
-	        {
-	            int target = sc.nextInt();
-	                sb.append(upperBound(A, target) - lowerBound(A, target)).append(" ");
-	        }
-	        System.out.println(sb);
+            sb.append(map.getOrDefault(target, 0)).append(' ');
+        }
 
-	    }
-	
-	
-	    public static int upperBound(int[] arr, int target)
-	    {
-	        int left = 0;
-	        int right = arr.length;
-	        while(left<right)
-	        {
-	            int mid = (left+right)/2;
-	            if(target < arr[mid])
-	            {
-	                right = mid;
-	            }else left=mid + 1;
-	        }
-	        return left;
-	    }
-	    
-	    
-	    public static int lowerBound(int[] arr, int target)
-	    {
-	        int left = 0;
-	        int right = arr.length;
-	        while(left<right)
-	        {
-	            int mid = (left+right)/2;
-	            if(arr[mid]>=target)
-	            {
-	                right = mid;
-	            }else left = mid + 1;
-	        }
-	        return left;
-	    }
+        System.out.println(sb);
+    }
 }
