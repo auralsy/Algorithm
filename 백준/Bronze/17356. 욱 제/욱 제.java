b7a0bd4865761c3fc40b3205a5e5000410d31fa2
aura_230 <147060267+auralsy@users.noreply.github.com>
2025-12-26 14:11:29 +0900

    [Bronze IV] Title: 욱 제, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/17356. 욱 제/욱 제.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        //Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        double wjr = 1/(1+(Math.pow(10, ((b-a)/400.0))));
+
+        System.out.println(wjr);
+        }
+    }
