c75602d1aed0815f14571ed6786c4b6c1976efdd
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-01 16:49:07 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int min = a;
+        
+        if(min > b)
+            min = b;
+
+        System.out.println(min);
+    }
+    }
