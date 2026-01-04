1e8bfaa158a5fd81a054fc08313841c7e77c0b2c
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-04 19:24:34 +0900

    [Bronze III] Title: 오븐 시계, Time: 108 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,25 +1,28 @@
+import java.io.*;
 import java.util.*;
-import java.lang.Math;
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
 
-        int h = sc.nextInt();
-        int m = sc.nextInt();
-        int c = sc.nextInt();
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean arrive;
+    static boolean visited[];
+    public static void main(String args[]) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int ph = h+((m+c)/60);
-        int pm = (m+c)%60;
-        
-        if(ph >= 24)
-        {
-            System.out.printf("%d %d", ph%24, pm);
-        }
-        else 
-        {
-            System.out.printf("%d %d", ph, pm);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int hour = Integer.parseInt(st.nextToken());
+        int minu = Integer.parseInt(st.nextToken());
+
+        int ctm = Integer.parseInt(br.readLine());
+
+        int minuR = (minu + ctm)%60;
+        int hourR = (minu + ctm)/60 + hour;
+
+        if(hourR >= 24)
+            hourR -= 24;
+
+        System.out.printf("%d %d", hourR, minuR);
         }
     }
-}
