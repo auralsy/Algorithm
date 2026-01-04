313ebf64079ab60ab93196ccf03e77674cd324d5
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-04 20:29:27 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 256 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -2,56 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    /*
-    public static int GCD(int a, int b) {
-        if(b == 0)
-            return a;
-        else
-            return GCD(b, a%b);
-    }
-*/
-    public static boolean check_prime(long a) {
-        if(a < 2)
-            return false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        for(long i = 2; i*i <= a; i++)
-        {
-            if(a % i == 0)
-            {
-                return false;
+        boolean[] isPrime = new boolean[1000001];
+
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= 1000000; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= 1000000; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return true;
-    }
 
-    public static void main(String args[]) throws IOException {
-        //Scanner sc = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
 
-        boolean[] ech = new boolean[1000001];
-
-        for(int i = 0; i <= 1000000; i++)
-        {
-            if(check_prime(i))
-                ech[i] = true;
-        }
-
-        int n = Integer.parseInt(br.readLine());
-
-        while(n --> 0)
-        {
-            int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
             int count = 0;
-            
-            for(int j = 2; j <= t/2; j++)
-            {
-                if(ech[j] && ech[t-j])
-                {
+
+            for (int i = 2; i <= n / 2; i++) {
+                if (isPrime[i] && isPrime[n - i]) {
                     count++;
                 }
             }
-            System.out.println(count);
+            sb.append(count).append('\n');
         }
-        }
+
+        System.out.println(sb);
     }
+}
