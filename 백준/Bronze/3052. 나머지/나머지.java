5af18192c37659877f9076cc0efa36ae1f85496a
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-05 13:27:44 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,19 +1,23 @@
+import java.io.*;
 import java.util.*;
-import java.lang.Math;
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
 
-        HashSet<Integer> hs = new HashSet<Integer>();
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
+
+        HashSet<Integer> hs = new HashSet<>();
 
         for(int i = 0; i < 10; i++)
         {
-            int k = sc.nextInt();
-            hs.add(k%42);
+            int n = Integer.parseInt(br.readLine());
+
+            int r = n%42;
+
+            hs.add(r);
         }
 
         System.out.println(hs.size());
+        }
     }
-}
