7a48dcb99c897c25bbcd7212c0697b2b6c921314
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-05 13:21:47 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 200 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,24 +1,27 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i <= n; i++)
         {
-            for(int j = 1; j < n-i; j++)
+            for(int j = 0; j < (n-i); j++)
             {
                 System.out.print(" ");
             }
-            for(int j = 0; j <= i; j++)
+
+            for(int k = 0; k < i; k++)
             {
                 System.out.print("*");
             }
+
             System.out.println();
         }
+        }
     }
-}
