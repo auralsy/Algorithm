e25c6f6167b53b70980901319b8154a1c9ade40a
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-12 07:24:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,21 +1,18 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[]) throws IOException
-    {
+public class Main {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        String Line = br.readLine();
-        StringTokenizer st = new StringTokenizer(Line);
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
 
-        bw.write((a+b+c) + "");
-        bw.flush();
+        System.out.println(a+b+c);
     }
-}
+    }
