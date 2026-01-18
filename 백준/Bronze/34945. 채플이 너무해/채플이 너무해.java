a973410f1af3e0fe39dd0aac97006a3c1230ae6a
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-18 10:25:16 +0900

    [Bronze V] Title: 채플이 너무해, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n >= 6)
+            System.out.println("Success!");
+        else
+            System.out.println("Oh My God!");
+    }
+    }
