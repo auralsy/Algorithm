36babb43cd7053873d865a659020b054191a1df5
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-25 22:49:47 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -2,34 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
 
-    public static boolean checkPrime(int n)
-    {
-        if(n < 2)
-            return false;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 2; i*i <= n; i++)
+        int n = Integer.parseInt(st.nextToken());
+
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
         {
-            if(n % i == 0)
-                return false;
+            int a = Integer.parseInt(st.nextToken());
+
+            if(a == 1)
+                continue;
+            else if(a == 2)
+            {
+                count++;
+                continue;
+            }
+            else
+            {
+                if(checkPrime(a))
+                {
+                    count++;
+                }
+            }
         }
 
+        System.out.println(count);
+    }
+
+    static boolean checkPrime(int a)
+    {
+        for(int i = 2; i*i <= a; i++)
+        {
+            if(a%i == 0)
+            {
+                return false;
+            }
+        }
         return true;
     }
-
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt();
-        int result = 0;
-
-        for(int i = 0; i < t; i++)
-        {
-            int n = sc.nextInt();
-            
-            if(checkPrime(n))
-                result += 1;
-        }
-        System.out.println(result);
     }
-}
