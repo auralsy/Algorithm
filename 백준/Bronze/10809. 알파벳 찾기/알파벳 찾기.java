6dc11316272fb79a4edbefc255615cccbaa09b2e
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-26 18:09:36 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -2,32 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
 
-        int[] arr = new int[26];
+        int[] apb = new int[26];
+
         for(int i = 0; i < 26; i++)
         {
-            arr[i] = -1;
+            apb[i] = -1;
         }
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        hm.put('a', 0); hm.put('b', 1); hm.put('c', 2); hm.put('d', 3);
+        hm.put('e', 4); hm.put('f', 5); hm.put('g', 6); hm.put('h', 7);
+        hm.put('i', 8); hm.put('j', 9); hm.put('k', 10); hm.put('l', 11);
+        hm.put('m', 12); hm.put('n', 13); hm.put('o', 14); hm.put('p', 15);
+        hm.put('q', 16); hm.put('r', 17); hm.put('s', 18); hm.put('t', 19);
+        hm.put('u', 20); hm.put('v', 21); hm.put('w', 22); hm.put('x', 23);
+        hm.put('y', 24); hm.put('z', 25);
+
 
         for(int i = 0; i < str.length(); i++)
         {
-            char c = str.charAt(i);
-
-            // a의 아스키코드 값 뺀 결과가 알파벳 별 위치
-            // 알파벳이 '처음' 나온 위치 기준이므로 -1일 때만 초기화
-            if(arr[c - 'a'] == -1)
-                arr[c - 'a'] = i;
+            if(apb[hm.get(str.charAt(i))] == -1)
+                apb[hm.get(str.charAt(i))] = i;
         }
 
-        for(int n : arr)
-        {
-            System.out.print(n + " ");
-        }
+        for(int i = 0; i < 26; i++)
+            sb.append(apb[i]).append(" ");
+
+        System.out.println(sb.toString());
     }
-}
+    }
