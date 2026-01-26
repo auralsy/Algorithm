fcbbfac9a086d6d520ca7271f9888adec5f939ae
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-26 18:50:50 +0900

    [Silver V] Title: 다리 놓기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,26 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int n = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < t; i++)
         {
-            int w = sc.nextInt();
-            int e = sc.nextInt();
-            
-            int k = Math.min(w, e-w);
-            
-            Long result = 1L;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
 
+            int k = Math.min(r, n-r);
+            long result = 1l;
+            
             for(int j = 0; j < k; j++)
             {
-                result *= (e-j);
+                result *= (n-j);
                 result /= (j+1);
             }
-            System.out.println(result);
+
+            sb.append(result).append('\n');
         }
+
+        System.out.println(sb);
     }
-}
+    }
