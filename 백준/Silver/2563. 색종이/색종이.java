94ae72aa4b92ee2616025b25ee37ad6e6fa32e46
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-26 17:51:44 +0900

    [Silver V] Title: 색종이, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -2,47 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-
-        // 도화지 생성
-        boolean[][] bp = new boolean[100][100];
-
-        // 좌측 변, 아래 변 위치
-        int[][] arr = new int[n][2];
+        boolean[][] arr = new boolean[100][100];
+        int count = 0;
 
         for(int i = 0; i < n; i++)
         {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
-        }
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 도화지 칠하기
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = arr[i][0]; j <= arr[i][0]+9; j++)
+            int ls = Integer.parseInt(st.nextToken());
+            int bs = Integer.parseInt(st.nextToken());
+
+            for(int j = ls; j <= (ls+9); j++)
             {
-                for(int t = arr[i][1]; t <= arr[i][1]+9; t++)
+                for(int t = bs; t <= (bs+9); t++)
                 {
-                    bp[t][j] = true;
+                    arr[t][j] = true;
                 }
             }
         }
-
-        int sum = 0;
 
         for(int i = 0; i < 100; i++)
         {
             for(int j = 0; j < 100; j++)
             {
-                if(bp[i][j] == true)
-                    sum += 1;
+                if(arr[i][j] == true)
+                    count++;
             }
         }
 
-        System.out.println(sum);
+        System.out.println(count);
     }
-}
+    }
