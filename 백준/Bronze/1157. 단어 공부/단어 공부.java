80c3a11c34de37c56260bd4c46c87b14f80375af
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-01-27 21:53:20 +0900

    [Bronze I] Title: 단어 공부, Time: 232 ms, Memory: 20164 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -2,36 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        String str = sc.nextLine();
-        // A->65, a->97
-
+        String str = br.readLine();
         int[] count = new int[26];
-
+        
         for(int i = 0; i < str.length(); i++)
         {
             if(str.charAt(i) < 97)
-            {
                 count[str.charAt(i) - 65]++;
-            }
-            else
-            {
+            else 
                 count[str.charAt(i) - 97]++;
-            }
         }
-
+        
         int max = 0;
+        int index = 0;
         boolean check = false;
-        int ind = 0;
-
+        
         for(int i = 0; i < 26; i++)
         {
             if(count[i] > max)
             {
                 max = count[i];
-                ind = i;
+                index = i;
                 check = false;
             }
             else if(count[i] == max && max != 0)
@@ -39,15 +35,13 @@ public class Main {
                 check = true;
             }
         }
-
+        
         if(!check)
         {
-            char c = (char)(ind + 65);
+            char c = (char)(index+65);
             System.out.println(c);
         }
         else
-        {
             System.out.println("?");
-        }
     }
-}
+    }
