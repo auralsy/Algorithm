b7a23282cd5c36bcbc5201c24de47096ccf7801b
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-02-05 19:15:51 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) 
+    {
+        double answer = price;
+        
+        if(answer >= 500000)
+            answer *= 0.8;
+        else if(answer >= 300000)
+            answer *= 0.9;
+        else if(answer >= 100000)
+            answer *= 0.95;
+        
+        return (int)answer;
+    }
+}
