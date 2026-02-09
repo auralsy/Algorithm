ad2761ce67e0a5ca3d727031c572fe5131bee14b
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-02-09 16:50:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.59 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        
+        String[] arr = my_string.split("");
+        
+        Arrays.sort(arr);
+        String answer = "";
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
