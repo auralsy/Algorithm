de5fda13f74dc8a42b36956917be804f0683519b
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-05-18 11:18:53 +0900

    [level 0] Title: [PCCE 기출문제] 1번 / 문자 출력, Time: 161.58 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/340207. ［PCCE 기출문제］ 1번 ／ 문자 출력/［PCCE 기출문제］ 1번 ／ 문자 출력.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3" + System.lineSeparator()+ "2" + System.lineSeparator() + "1");
+        System.out.println(message);
+    }
+}
+
