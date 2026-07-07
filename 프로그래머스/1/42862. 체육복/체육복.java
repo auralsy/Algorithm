48053d4023173646503722d97ba7cdb215192255
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-07-08 07:05:49 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,44 @@
+import java.util.*;
+// 0을 기준점으로. -1은 도난당한 상태. 1은 여벌 가져온 경우.
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        int[] students = new int[n+2];
+        
+        for(int i = 0; i < lost.length; i++)
+        {
+            students[lost[i]]--;
+        }
+        
+        for(int i = 0; i < reserve.length; i++)
+        {
+            students[reserve[i]]++;
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(students[i] == -1)
+            {
+                if(students[i-1] == 1)
+                {
+                    students[i] = 0;
+                    students[i-1] = 0;
+                }
+                else if(students[i+1] == 1)
+                {
+                    students[i] = 0;
+                    students[i+1] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(students[i] >= 0)
+                answer++;
+        }
+        
+        return answer;
+    }
+}
