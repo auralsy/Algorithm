32310fd3557e89de799c862d7a8d9fbd232ccc7a
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-07-28 11:40:41 +0900

    [level 0] Title: l로 만들기, Time: 629.66 ms, Memory: 386 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++)
+        {
+            char c = myString.charAt(i);
+            
+            if(c - 'l' <= 0)
+                answer += "l";
+            else
+                answer += Character.toString(c);
+        }
+        
+        return answer;
+    }
+}
