7514df48a3a63c1191f8a7002598bb6fb599181c
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-08-19 09:36:37 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 2.27 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int ind = 0;
+        
+        for(int i = 0; i < seoul.length; i++)
+        {
+            if(seoul[i].equals("Kim"))
+            {
+                ind = i;
+                break;
+            }
+        }
+        
+        answer = "김서방은 " + ind + "에 있다";
+        
+        return answer;
+    }
+}
