396299dad1c649781942d56cb9d4c81c34b22398
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-08-20 13:03:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 6.49 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        for(long i = 1; i*i <= n; i++)
+        {
+            if(i*i == n)
+            {
+                answer = i;
+                break;
+            }
+        }
+        
+        if(answer == 0)
+            answer = -1;
+        else
+        {
+            answer = (answer+1) * (answer+1);
+        }
+        
+        return answer;
+    }
+}
